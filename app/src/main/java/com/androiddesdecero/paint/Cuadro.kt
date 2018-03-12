76479443e13ec47021fd0fda4148d89bf6bfbe83
Alberto <info@androiddesdecero.com>
2018-03-12 12:43:38 +0100

    Primer

---
+++ app/src/main/java/com/androiddesdecero/paint/Cuadro.kt
@@ -0,0 +1,12 @@
+package com.androiddesdecero.paint
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by albertopalomarrobledo on 12/3/18.
+ */
+class Cuadro @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr) {
+}
